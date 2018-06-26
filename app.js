2d64abd9c362420a3738762b84e4f0b364e1a732
Ben Parman <benparman@gmail.com>
2018-06-26 10:53:14 -0600

    added function to return starred segments.  Temporarily over rate limit on original access token, so having to work with different data on a temporary token for a dummy Strava account.  Writing loop for function to retrieve starred segments, and will duplicate for function to get KOM's.

--- app.js
+++ app.js
@@ -2,8 +2,42 @@
 
 const STATE = {
   userID: 238423,
-  access_token: '283e21d3a127a4cb5ca4ad4608272825c5a35332',
+  // access_token: '283e21d3a127a4cb5ca4ad4608272825c5a35332', <--over rate limit
+  access_token: '9f88334edeb57cc3c19505578cf5a4a60bc1e890',
+  KOMS: [],
+  pagenum: 1,
+  lastPush: 1,
+  starred: []
 };
+
+//----------Retrieves Authenticated User's Starred Segments----------//
+function getStarred() {
+  let url = 'https://www.strava.com/api/v3/segments/starred';
+  const settings = {
+    url: url,
+    data: {
+      access_token:STATE.access_token,
+      per_page: 5,
+      page: STATE.pagenum
+    },
+    success: function(data) {
+      STATE.starred.push(data);
+      console.log(data);
+    },
+    error: function() {
+      console.log('there was a problem with your request');
+    }
+  };
+  $.ajax(settings);
+}
+
+
+//----------Call API's----------//
+function getData() {
+  // getKoms();
+  getStarred();
+}
+
 //----------Retrieves Authenticated User's KOM's----------//
 function getKoms() {
   let url = `https://www.strava.com/api/v3/athletes/${STATE.userID}/koms?`;
@@ -12,11 +46,19 @@ function getKoms() {
     data: {
       id: STATE.userID,
       access_token: STATE.access_token,
-      per_page: 200,
-      page: 1
+      per_page: 50,
+      page: STATE.pagenum
     },
     success: function(data) {
+      STATE.KOMS.push(data);
+      if (!(data.length === 0)) {
+        STATE.fetch = true;
+      }
       console.log('Successfully retrieved authenticated user\'s KOM\'s', data);
+      STATE.lastPush = data.length;
+      STATE.pagenum ++;
+      STATE.KOMS.push(data);
+      return data;
     },
     error: function() {
       console.log('error');
@@ -25,4 +67,4 @@ function getKoms() {
   $.ajax(settings);
 }
 
-$(document).ready(getKoms);
+$(document).ready(getData);
