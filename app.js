471d31f14b4e19ec3ae627aade701f48bd2f4957
Ben Parman <benparman@gmail.com>
2018-06-28 12:59:45 -0600

    refactored getStarredSegments() function.  Working properly, loops through function until conditional logic is no longer met, returning 200 elements per request until all have been retrieved

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const STATE = {
-  userID: 238423, //personal
-  // userID: 1227925, //dummy
-  access_token: '283e21d3a127a4cb5ca4ad4608272825c5a35332', //personal
-  // access_token: '9f88334edeb57cc3c19505578cf5a4a60bc1e890', //dummy
+  // userID: 238423, //personal
+  userID: 1227925, //dummy
+  // access_token: '283e21d3a127a4cb5ca4ad4608272825c5a35332', //personal
+  access_token: '9f88334edeb57cc3c19505578cf5a4a60bc1e890', //dummy
   koms: [],
   starred: [],
   starredPage: []
@@ -14,8 +14,8 @@ const STATE = {
 function getData() {
   $('#getButton').click(function(event){ 
     event.preventDefault();
-    getKoms();
-    // getStarredSegments();
+    // getKoms();
+    getStarredSegments();
   });
 }
 //----------Retrieves Authenticated User's Leading Segments----------//
@@ -50,31 +50,32 @@ function getKoms() {
 }
 //----------Retrieves Authenticated User's Starred Segments----------//
 function getStarredSegments() {
-  for(let i = 0; i<10 && STATE.starPushCount > 0; i++) { //DO NOT change && to || or remove <10 safety net!!! Will kill API!
+  let pageCounter = 1;
+  starredAPI();
+  function starredAPI(){
     let url = 'https://www.strava.com/api/v3/segments/starred';
     const settings = {
       url: url,
       data: {
         access_token:STATE.access_token,
-        per_page: 25,
-        page: STATE.starPageNum
+        per_page: 200,
+        page: pageCounter
       },
       success: function(data) {
         data.forEach(element => {
           STATE.starred.push(element);
         });
-        STATE.starPushCount = data.length;
-        console.log(data.length);
+        pageCounter++;
+        if(data.length === 200) {
+          starredAPI();
+        }
       },
-      error: function() {
-        console.log('there was a problem with your request');
+      error: function(res) {
+        console.log('Error! Status ',res.status);
       }
     };
-    console.log(settings);
-    // $.ajax(settings);
-    STATE.starPageNum++;
+    $.ajax(settings);
   }
-  console.log('Starred Segments: ', STATE.starred);
 }
 
 $(document).ready(getData);
