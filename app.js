0f03ad4d35621a2ca0203b00c174a0320a8ef3fc
Ben Parman <benparman@gmail.com>
2018-06-27 23:10:15 -0600

    Get KOM function is working with loop, had conditional logic in the wrong place

--- app.js
+++ app.js
@@ -1,70 +1,94 @@
 'use strict';
 
 const STATE = {
-  userID: 238423,
-  // access_token: '283e21d3a127a4cb5ca4ad4608272825c5a35332', <--over rate limit
-  access_token: '9f88334edeb57cc3c19505578cf5a4a60bc1e890',
-  KOMS: [],
-  pagenum: 1,
-  lastPush: 1,
-  starred: []
+  userID: 238423, //personal
+  // userID: 1227925, //dummy
+  access_token: '283e21d3a127a4cb5ca4ad4608272825c5a35332', //personal
+  // access_token: '9f88334edeb57cc3c19505578cf5a4a60bc1e890', //dummy
+  leader: [],
+  leaderPage: 1,
+  leaderPushCount: 1,
+  starred: [],
+  starredPage: []
 };
 
-//----------Retrieves Authenticated User's Starred Segments----------//
-function getStarred() {
-  let url = 'https://www.strava.com/api/v3/segments/starred';
-  const settings = {
-    url: url,
-    data: {
-      access_token:STATE.access_token,
-      per_page: 5,
-      page: STATE.pagenum
-    },
-    success: function(data) {
-      STATE.starred.push(data);
-      console.log(data);
-    },
-    error: function() {
-      console.log('there was a problem with your request');
-    }
-  };
-  $.ajax(settings);
-}
-
-
-//----------Call API's----------//
+//----------Listen for get data button click----------//
 function getData() {
-  // getKoms();
-  getStarred();
+  $('#getButton').click(function(event){ 
+    event.preventDefault();
+    loopGetLeader();
+  });
+}
+// getStarredSegments();
+
+
+//----------Retrieves Authenticated User's Leading Segments----------//
+function loopGetLeader() {
+  let pushCounter = 1;
+  let pageCounter = 1;
+  let i = 0;
+  // while(i<5 && pushCounter >0){
+  getLeader();
+  i++;
+  // pageCounter++;
+  // }
+  function getLeader() {
+    let url = `https://www.strava.com/api/v3/athletes/${STATE.userID}/koms?`;
+    const settings = {
+      url: url,
+      data: {
+        id: STATE.userID,
+        access_token: STATE.access_token,
+        per_page: 50,
+        page: pageCounter
+      },
+      success: function(data) {
+        STATE.leader.push(data);
+        pushCounter = data.length;
+        pageCounter++;
+        console.log('pushCounter = ', pushCounter);
+        console.log('Returned Data = ', data);
+
+        if(i<5 && pushCounter > 0) {
+          getLeader();
+        }
+      },
+      error: function(res) {
+        console.log('Error! Status ',res.status);
+      }
+    };
+    $.ajax(settings); 
+    console.log('pageCounter = ', pageCounter);
+  }
 }
 
-//----------Retrieves Authenticated User's KOM's----------//
-function getKoms() {
-  let url = `https://www.strava.com/api/v3/athletes/${STATE.userID}/koms?`;
-  const settings = {
-    url: url,
-    data: {
-      id: STATE.userID,
-      access_token: STATE.access_token,
-      per_page: 50,
-      page: STATE.pagenum
-    },
-    success: function(data) {
-      STATE.KOMS.push(data);
-      if (!(data.length === 0)) {
-        STATE.fetch = true;
+//----------Retrieves Authenticated User's Starred Segments----------//
+function getStarredSegments() {
+  for(let i = 0; i<10 && STATE.starPushCount > 0; i++) { //DO NOT change && to || or remove <10 safety net!!! Will kill API!
+    let url = 'https://www.strava.com/api/v3/segments/starred';
+    const settings = {
+      url: url,
+      data: {
+        access_token:STATE.access_token,
+        per_page: 25,
+        page: STATE.starPageNum
+      },
+      success: function(data) {
+        data.forEach(element => {
+          STATE.starred.push(element);
+        });
+        STATE.starPushCount = data.length;
+        console.log(data.length);
+      },
+      error: function() {
+        console.log('there was a problem with your request');
       }
-      console.log('Successfully retrieved authenticated user\'s KOM\'s', data);
-      STATE.lastPush = data.length;
-      STATE.pagenum ++;
-      STATE.KOMS.push(data);
-      return data;
-    },
-    error: function() {
-      console.log('error');
-    }
-  };
-  $.ajax(settings);
+    };
+    console.log(settings);
+    // $.ajax(settings);
+    STATE.starPageNum++;
+  }
+  console.log('Starred Segments: ', STATE.starred);
 }
 
 $(document).ready(getData);
