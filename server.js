efa8c06759e8d77fa1e266f701dceaeea6f02ad9
Ben Parman <benparman@gmail.com>
2018-06-29 15:57:32 -0600

    Working sort function that takes array, method(not currently using),sort criteria(ie state), and parent(ie segment).  Successfully returning data in Ascending alphabetical order, need to duplicate for descending.

--- server.js
+++ server.js
@@ -13,12 +13,24 @@ const app = express();
 app.use(morgan('common'));
 app.use(express.json());
 
+//Get Segments
 app.get('/segments', (req, res) => {
   Segments
     .find()
     .then(segments => {
       res.json(segments.map(segments => segments.serialize()));
     })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'There was an error with your request' });
+    });
+});
+
+//Get Segment by ID
+app.get('/segments/:id', (req, res) => {
+  Segments
+    .findById(req.params.id)
+    .then(segments => res.json(segments.serialize()))
     .catch(err => {
       console.error(err);
       res.status(500).json({ error: 'There was an error with your request' });
